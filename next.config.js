00740da778e315f2886072f16c54ee387b21cfcd
Odoyo Cliff <odoyoallen@gmail.com>
2026-05-18 18:12:25 +0300

    Add basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -9,3 +9,14 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/finos-mvp3',
+  assetPrefix: '/finos-mvp3/',
+  images: {
+    unoptimized: true
+  }
+}
+
+module.exports = nextConfig
