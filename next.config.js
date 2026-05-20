1227dc138724c7b9fbadeefb215081d1526a24a3
Odoyo Cliff <odoyoallen@gmail.com>
2026-05-20 15:15:17 +0300

    Configure Next.js for GitHub Pages static export

--- next.config.js
+++ next.config.js
@@ -1,22 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true
-  },
+  images: { unoptimized: true },
   basePath: '/finos-mvp3',
   assetPrefix: '/finos-mvp3/',
-}
-
-module.exports = nextConfig
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/finos-mvp3',
-  assetPrefix: '/finos-mvp3/',
-  images: {
-    unoptimized: true
-  }
 }
 
 module.exports = nextConfig
